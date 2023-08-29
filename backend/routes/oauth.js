6ca5fcac8cacbf21b6aeea28a22d8f8ad257e243
Ujjwal6016 <ujjvalmishra6016@gmail.com>
2023-08-29 23:59:32 +0530

    code

--- backend/routes/oauth.js
+++ backend/routes/oauth.js
@@ -14,7 +14,7 @@ const BASE_URL = process.env.BASE_URL
 passport.use(new GoogleStrategy({
   clientID: process.env.GOOGLE_CLIENT_ID,
   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  callbackURL: '/oauth2/redirect/google',
+  callbackURL: process.env.GOOGLE_REDIRECT_URL,
   scope: ['profile', 'email']
 }, async (accessToken, refreshToken, profile, done) => {
   try {
