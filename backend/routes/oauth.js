896fb31f5b0fe8289a183fe8b79054eda44bc017
Ujjwal6016 <ujjvalmishra6016@gmail.com>
2023-10-17 21:33:39 +0530

    code

--- backend/routes/oauth.js
+++ backend/routes/oauth.js
@@ -41,7 +41,7 @@ const router = express.Router();
 passport.use(new GoogleStrategy({
   clientID: process.env.GOOGLE_CLIENT_ID,
   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  callbackURL: "/oauth2/redirect/google", 
+  callbackURL: process.env.GOOGLE_REDIRECT_URL, 
   scope: ['profile', 'email']
 }, async (accessToken, refreshToken, profile, done) => {
   try {
